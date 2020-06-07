1644ada1eae97a71b731dac71f9d0220eaa67b00
IceMaple <a13259768671@163.com>
2020-06-07 19:01:50 +0800

    工程代码

---
+++ src/main/java/com/mabf/configaration/RestTemplateConfig.java
@@ -0,0 +1,23 @@
+package com.mabf.configaration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateConfig {
+
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(15000);
+        return factory;
+    }
+}
