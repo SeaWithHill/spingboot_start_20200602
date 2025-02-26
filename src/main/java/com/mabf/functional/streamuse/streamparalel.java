64740edb2333725556b735a46e636eddc0fd15ce
SeaWithHill <a13259768671@163.com>
2025-02-26 22:43:54 +0800

    feat(1):apartment

--- src/main/java/com/mabf/functional/streamuse/streamparalel.java
+++ src/main/java/com/mabf/functional/streamuse/streamparalel.java
@@ -1,8 +1,6 @@
 package com.mabf.functional.streamuse;
 
 import java.util.Optional;
-import java.util.function.BinaryOperator;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class streamparalel {
@@ -11,17 +9,7 @@ public class streamparalel {
         Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         Optional<Integer> reduce = integerStream
                 .parallel()
-                .filter(new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer integer) {
-                return integer > 5;
-            }
-        }).reduce(new BinaryOperator<Integer>() {
-            @Override
-            public Integer apply(Integer integer, Integer integer2) {
-                return integer + integer2;
-            }
-        });
+                .filter(integer -> integer > 5).reduce((integer, integer2) -> integer + integer2);
         reduce.ifPresent(integer -> System.out.println(integer));
 
     }
