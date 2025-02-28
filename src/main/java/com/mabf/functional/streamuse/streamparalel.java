838bdc12ba9d304340509383ed3834989082ac5e
seawithhill <seawithhill@gmail.com>
2025-02-28 16:49:35 +0800

    fix(1):workspace

--- src/main/java/com/mabf/functional/streamuse/streamparalel.java
+++ src/main/java/com/mabf/functional/streamuse/streamparalel.java
@@ -5,12 +5,13 @@ import java.util.stream.Stream;
 
 public class streamparalel {
     public static void main(String[] args) {
-
         Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         Optional<Integer> reduce = integerStream
                 .parallel()
                 .filter(integer -> integer > 5).reduce((integer, integer2) -> integer + integer2);
         reduce.ifPresent(integer -> System.out.println(integer));
-
+        // TODO BY MaBinfeng record in 2025/2/27: 需要优化
+        // TODO BY MaBinfeng record in 2025/2/27:
+        System.out.println(reduce);
     }
 }
